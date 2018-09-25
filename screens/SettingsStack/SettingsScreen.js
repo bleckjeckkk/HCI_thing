510db6e86643668a0b9540962a972797534bed58
Dan <danjoaquinchin@gmail.com>
2018-09-25 14:37:42 +0800

    wedf

--- screens/SettingsStack/SettingsScreen.js
+++ screens/SettingsStack/SettingsScreen.js
@@ -19,14 +19,22 @@ export default class SettingsScreen extends React.Component {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
     return (
-      <View style={styles.helpContainer}>
-        <TouchableOpacity onPress={this._handleFAQPress} style={styles.helpLink}>
-          <Text style={styles.helpLinkText}>FAQ</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this._handleContactUsPress} style={styles.helpLink}>
-          <Text style={styles.helpLinkText}>Contact Us</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView style={{padding : 20}}>
+        <Text>
+          Did you ever hear the tragedy of YourCCS? I thought not.
+          It’s not a story the IT would tell you. It’s a ComSci legend. YourCCS was a mobile application of the ComSci, so powerful and so realtime it could use the Internet to influence the Realtime Database to create information…
+          It had such a knowledge of the tips and tricks that it could even keep the ones the devs cared about from getting an F. The tips and tricks is a pathway to many abilities some consider to be unnatural. It became so powerful… the only thing the devs were afraid of was the app losing its power, which eventually, of course, it did.
+          Unfortunately, it downloaded the entire database everytime it ran, then the school WiFi made it worse, and gave him 30 seconds of boot up time. Ironic. It could save others from failure, but not itself.
+        </Text>
+        <View style={styles.helpContainer}>
+          <TouchableOpacity onPress={this._handleFAQPress} style={styles.helpLink}>
+            <Text style={styles.helpLinkText}>FAQ</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._handleContactUsPress} style={styles.helpLink}>
+            <Text style={styles.helpLinkText}>Contact Us</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     );
   }
 
